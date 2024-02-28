ab229bc907ae914fbdf1557d6d1aab370b80a747
sagarje6894 <sagargarje0077@gmail.com>
2024-02-28 11:15:40 +0530

    Data Ingestion Added

--- src/cnnClassifier/pipeline/stage_01_data_ingestion.py
+++ src/cnnClassifier/pipeline/stage_01_data_ingestion.py
@@ -1,7 +1,6 @@
-from cnnClassifier.config.configuration import ConfigurationManager
+from config.configuration import ConfigurationManager
 from cnnClassifier.components.data_ingestion import DataIngestion
 from cnnClassifier import logger
-
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -16,15 +15,15 @@ class DataIngestionTrainingPipeline:
         data_ingestion = DataIngestion(config=data_ingestion_config)
         data_ingestion.download_file()
         data_ingestion.extract_zip_file()
-        
 
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     try:
-        logger.info(f">>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = DataIngestionTrainingPipeline()
         obj.main()
-        logger.info(f">>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<\n\nx==========x")
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
         logger.exception(e)
         raise e
